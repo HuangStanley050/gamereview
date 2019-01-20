bf28967e345c65f4841e7b2949ede5d932b6d583
Stanley Huang <infamous_godhand@yahoo.com>
2019-01-20 04:19:54 +0000

    getting rid off react-redux-firebase for now

--- src/components/reviews/reviews.js
+++ src/components/reviews/reviews.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-//import { fetch_reviews } from "../../store/actions/fetchActions";
-import { compose } from "redux";
-import { firestoreConnect } from "react-redux-firebase";
+import { fetch_reviews } from "../../store/actions/fetchActions";
+//import { compose } from "redux";
+//import { firestoreConnect } from "react-redux-firebase";
 import "react-accessible-accordion/dist/fancy-example.css";
 import {
   Accordion,
@@ -12,34 +12,34 @@ import {
 } from "react-accessible-accordion";
 
 class Reviews extends Component {
-  // componentDidUpdate(prevProps) {
-  //   if (this.props.auth.isLogin !== prevProps.auth.isLogin) {
-  //     console.log("current props: ", this.props.auth.isLogin);
-  //     console.log("previoius props: ", prevProps.auth.isLogin);
-  //     this.props.get_reviews();
-  //   }
-  // }
+  componentDidUpdate(prevProps) {
+    if (this.props.auth.isLogin !== prevProps.auth.isLogin) {
+      console.log("current props: ", this.props.auth.isLogin);
+      console.log("previoius props: ", prevProps.auth.isLogin);
+      this.props.get_reviews();
+    }
+  }
 
   render() {
     let reviews = null;
-    // if (this.props.reviews.loading) {
-    //   reviews = <h1 className="red-text center-align">Loading...</h1>;
-    // } else {
-    //   reviews = this.props.reviews.reviews.map(review => {
-    //     return (
-    //       <Accordion key={review.id}>
-    //         <AccordionItem>
-    //           <AccordionItemTitle>
-    //             <h4>{review.title}</h4>
-    //           </AccordionItemTitle>
-    //           <AccordionItemBody>
-    //             <p>{review.content}</p>
-    //           </AccordionItemBody>
-    //         </AccordionItem>
-    //       </Accordion>
-    //     );
-    //   });
-    // }
+    if (this.props.reviews.loading) {
+      reviews = <h1 className="red-text center-align">Loading...</h1>;
+    } else {
+      reviews = this.props.reviews.reviews.map(review => {
+        return (
+          <Accordion key={review.id}>
+            <AccordionItem>
+              <AccordionItemTitle>
+                <h4>{review.title}</h4>
+              </AccordionItemTitle>
+              <AccordionItemBody>
+                <p>{review.content}</p>
+              </AccordionItemBody>
+            </AccordionItem>
+          </Accordion>
+        );
+      });
+    }
 
     return (
       <div className="container" style={{ marginTop: "40px" }}>
@@ -53,25 +53,21 @@ class Reviews extends Component {
   }
 }
 
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     get_reviews: () => dispatch(fetch_reviews())
-//   };
-// };
+const mapDispatchToProps = dispatch => {
+  return {
+    get_reviews: () => dispatch(fetch_reviews())
+  };
+};
 
 const mapStateToProps = state => {
   return {
     auth: state.auth,
 
-    reviews: state.firestore.ordered.reviews
+    reviews: state.reviews
   };
 };
 
-export default compose(
-  connect(mapStateToProps),
-  firestoreConnect([
-    {
-      collection: "reviews"
-    }
-  ])
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
 )(Reviews);
